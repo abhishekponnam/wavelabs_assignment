e32f3d06233d2eecebd7fd97b11b037f1b649c76
P Abhishek <abhishekponnamofficial07@gmail.com>
2023-04-12 07:18:40 +0530

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace  std;
+
+/* Add the required Helper Function */
+
+int main(){
+    vector<int> prices_month1 = {3,3,5,0,0,3,1,4};
+    /* Answer : 6 */
+
+    vector<int> prices_month2 = {1,2,3,4,5};
+    /* Answer : 4 */
+
+    vector<int> prices_month3 = {7,6,4,3,1};
+    /* Answer : 0 */
+
+    return 0;
+}
+
